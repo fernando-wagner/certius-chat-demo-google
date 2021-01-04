cef099e229048a647dcb12fda61fe89db17a1f57
wagner12 <wagner12@avaya.com>
2021-01-04 19:54:04 -0300

    Changed everything to certius-chat-demo-google

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('certius-chat-demo app is running!');
+    expect(page.getTitleText()).toEqual('certius-chat-demo-google app is running!');
   });
 
   afterEach(async () => {
